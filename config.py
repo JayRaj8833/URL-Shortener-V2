4d08e711870c416f816a4823a26723a40301c203
JayRaj ❤️ <87997138+JayRaj8833@users.noreply.github.com>
2024-12-25 16:11:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,64 +16,56 @@ def is_enabled(value, default):
 
 # Mandatory variables for the bot to start
 # API ID from https://my.telegram.org/auth
-API_ID = int(os.environ.get("API_ID","9840531"))
+API_ID = int(os.environ.get("API_ID", "2646548"))
 # API Hash from https://my.telegram.org/auth
-API_HASH = os.environ.get("API_HASH","2ac10377616df23907e134df14bdf08b")
-BOT_TOKEN = os.environ.get("BOT_TOKEN","5690405597:AAED5v6tifIwoc51juKUdi97bnEAPVscybA")  # Bot token from @BotFather
-ADMINS = (
-    [int(i.strip()) for i in os.environ.get("ADMINS").split(",")]
-    if os.environ.get("ADMINS")
-    else []
-)
+API_HASH = os.environ.get("API_HASH", "72e5b96d9aa943df98ec71241b11c7ac")
+BOT_TOKEN = os.environ.get("BOT_TOKEN",
+                           "7672062736:AAHOzTJGjKNOEfhRhM7QxhKCdZvtcCzVKdE"
+                           )  # Bot token from @BotFather
+ADMINS = ([int(i.strip()) for i in os.environ.get("ADMINS").split(",")]
+          if os.environ.get("ADMINS") else [])
 
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Tamizhmasters")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Short2Earn")
 DATABASE_URL = os.environ.get(
-    "DATABASE_URL", ""
+    "DATABASE_URL",
+    "mongodb+srv://short2earnlive:UF5Bw9HMt9qFYHBC@short2earn.gztsr.mongodb.net/?retryWrites=true&w=majority&appName=Short2Earn"
 )  # mongodb uri from https://www.mongodb.com/
-OWNER_ID = int(os.environ.get("OWNER_ID", "960432019"))  # id of the owner
+OWNER_ID = int(os.environ.get("OWNER_ID", "872623545"))  # id of the owner
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
- 
+
 #  Optionnal variables
-LOG_CHANNEL = int(
-    os.environ.get("LOG_CHANNEL", "-1001961486490")
-)  # log channel for information about users
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", False)  # For Force Subscription
-BROADCAST_AS_COPY = is_enabled(
-    (os.environ.get("BROADCAST_AS_COPY", "True")), True
-)  # true if forward should be avoided
-IS_PRIVATE = is_enabled(
-    os.environ.get("IS_PRIVATE", "False"), "False"
-)  # true for private use and restricting users
-SOURCE_CODE = os.environ.get(
-    "SOURCE_CODE", "https://jaymehta.pro"
-)  # for upstream repo
+LOG_CHANNEL = int(os.environ.get(
+    "LOG_CHANNEL",
+    "-1002345671291"))  # log channel for information about users
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL",
+                                False)  # For Force Subscription
+BROADCAST_AS_COPY = is_enabled((os.environ.get("BROADCAST_AS_COPY", "True")),
+                               True)  # true if forward should be avoided
+IS_PRIVATE = is_enabled(os.environ.get("IS_PRIVATE", "False"),
+                        "False")  # true for private use and restricting users
+SOURCE_CODE = os.environ.get("SOURCE_CODE",
+                             "https://jaymehta.pro")  # for upstream repo
 # image when someone hit /start
-WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", "")
-LINK_BYPASS = is_enabled(
-    (os.environ.get("LINK_BYPASS", "False")), False
-)  # if true, urls will be bypassed
+WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE",
+                               "https://short2earn.live/img/Short2Earn.png")
+LINK_BYPASS = is_enabled((os.environ.get("LINK_BYPASS", "False")),
+                         False)  # if true, urls will be bypassed
 # your shortener site domain
 BASE_SITE = os.environ.get("BASE_SITE", "short2earn.live")
 
 # For Admin use
 CHANNELS = is_enabled((os.environ.get("CHANNELS", "True")), True)
-CHANNEL_ID = (
-    [int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(",")]
-    if os.environ.get("CHANNEL_ID")
-    else []
-)
+CHANNEL_ID = ([
+    int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(",")
+] if os.environ.get("CHANNEL_ID") else [])
 
-DE_BYPASS = (
-    [i.strip() for i in os.environ.get("DE_BYPASS").split(",")]
-    if os.environ.get("DE_BYPASS")
-    else []
-)
+DE_BYPASS = ([i.strip() for i in os.environ.get("DE_BYPASS").split(",")]
+             if os.environ.get("DE_BYPASS") else [])
 DE_BYPASS.append("mdisk.me")
 
 FORWARD_MESSAGE = is_enabled(
-    (os.environ.get("FORWARD_MESSAGE", "True")), True
-)  # true if forwardd message to converted by reposting the post
-
+    (os.environ.get("FORWARD_MESSAGE", "True")),
+    True)  # true if forwardd message to converted by reposting the post
 
 WEB_SERVER = is_enabled(os.environ.get("WEB_SERVER", "True"), True)
 PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "240"))
